da6ef9c310873ffdb05a41833a46ecdcb5480f6f
abbiec123456 <l00196596@atu.ie>
2025-11-11 15:41:50 +0000

    image id

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -25,10 +25,20 @@ resource "aws_security_group" "web_sg" {
 
   tags = { Name = "web-sg" }
 }
+# Fetch latest Ubuntu 22.04 LTS AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
 
 resource "aws_instance" "web" {
   for_each               = var.public_subnets
-  ami                    = var.image_id
+  ami                    = var.image_id != "" ? var.image_id : data.aws_ami.ubuntu.id
   instance_type          = "t3.micro"
   subnet_id              = each.value.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
